cd7f1f9058a8e7e1a2f163e37ed0f76298f7889e
Scott Bailey <sbailey@anaconda.com>
2022-03-01 08:39:48 -0700

    CMAKE_ARGS is meh. :-( Fix here for now.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,11 @@ cd Build
 if errorlevel 1 exit 1
 
 :: Generate the build files.
-cmake .. -G"Ninja" %CMAKE_ARGS%
+cmake .. -G"Ninja" %CMAKE_ARGS% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release
+
 
 :: Build and install.
 ninja install
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,10 @@ mkdir -p Build
 cd Build || exit 1
 
 # Generate the build files.
-cmake .. -G"Ninja" ${CMAKE_ARGS}
+cmake .. -G"Ninja" ${CMAKE_ARGS} \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release
 
 # Build and install.
 ninja install || exit 1
